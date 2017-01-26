30bea24f3c74d33308fc68030bcc1bd8db899fab
Robetron <vishalsnedan@gmail.com>
2017-01-26 23:02:28 +0530

    fixed errors

--- Calculator.java
+++ Calculator.java
@@ -4,10 +4,10 @@ class Calculator {
         return n1+n2;
     }
     public static int mul(int n1, int n2){
-        return n1+n2;
+        return n1*n2;
     }
     public static int div(int n1, int n2){
-        return n1+n2;
+        return n1/n2;
     }
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
