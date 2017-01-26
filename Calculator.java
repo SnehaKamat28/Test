5d23c0ca1c6fe7f1756a07e6e366a295b0056fa1
Robetron <vishalsnedan@gmail.com>
2017-01-26 22:30:03 +0530

    Basic Calculator

---
+++ Calculator.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class Calculator {
+    public static int add(int n1, int n2){
+        return n1+n2;
+    }
+    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int choice,n1,n2;
+        do{
+            System.out.println("1:+\t2:-\t3:*\t4:/t0:Exit");
+            choice=in.nextInt();
+            System.out.print("Enter nos: ");
+            n1=in.nextInt();
+            n2=in.nextInt();
+            switch(choice){
+                case 1: System.out.println(add(n1, n2));  
+                        break;
+            }
+        }while(choice!=0);
+    }
+}
