e5a496ed864c0320ec71c57e3c21443595bc0bb2
Terence411 <loboterence4@gmail.com>
2017-01-28 11:17:07 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,25 +1,30 @@
 import java.util.Scanner;
-class Calculator {
-    public static int add(int n1, int n2){
-        return n1+n2;
+class Calculator 
+{   public static int add(int n1, int n2) 
+    {   return n1+n2;
     }
-    public static int mul(int n1, int n2){
-        return n1*n2;
+    public static int sub(int n1, int n2)
+    {   return n1-n2;
     }
-    public static int div(int n1, int n2){
-        return n1/n2;
+    public static int mul(int n1, int n2)
+    {   return n1*n2;
     }
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+    public static int div(int n1, int n2)
+    {   return n1/n2;
+    }
+    public static void main(String[] args) 
+    {   Scanner in = new Scanner(System.in);
         int choice,n1,n2;
-        do{
-            System.out.println("1:+\t2:-\t3:*\t4:/\t0:Exit");
+        do
+        {   System.out.println("1:+\t2:-\t3:*\t4:/\t0:Exit");
             choice=in.nextInt();
             System.out.print("Enter nos: ");
             n1=in.nextInt();
             n2=in.nextInt();
-            switch(choice){
-                case 1: System.out.println(add(n1, n2));  
+            switch(choice)
+            {   case 1: System.out.println(add(n1, n2));  
+                        break;
+                case 2: System.out.println(sub(n1, n2));  
                         break;
                 case 3: System.out.println(mul(n1, n2));  
                         break;
